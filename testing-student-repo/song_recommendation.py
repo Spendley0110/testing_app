9bf83229aff7b8469696fe002d3cf79354b056f9
Aprill235 <pencil018365@gmail.com>
2026-08-11 11:28:24 -0600

    LLM edits

--- testing-student-repo/song_recommendation.py
+++ testing-student-repo/song_recommendation.py
@@ -7,104 +7,50 @@ print("Path to dataset files:", path)
 #I moved the csv file to the free_coding folder
 
 def read_file(csv):
-    "reads the csv file dataset and splits it into lists by '\n'."
-    with open(csv, "r") as file:
+    with open(csv, 'r') as file:
         csv_file = file.readlines()
-        new_csv = []
-        id_list = []
-        for i in csv_file:
-            new_i = i.split(',')
-            if new_i[0] in id_list:
-                continue
-            new_csv.append(new_i)
-            id_list.append(new_i[0])
-    return new_csv
+    header = csv_file[0].strip().split(',')
+    data = [dict(zip(header, line.strip().split(','))) for line in csv_file[1:]]
+    return data
 
 def make_dictionary(csv_file):
-    dict_list = []
-    for i in range(1,len(csv_file)):
-        if len(csv_file[i]) == 23:
-            dict = {
-                'track_id': csv_file[i][0],
-                'track_name': csv_file[i][1],
-                'track_artist': csv_file[i][2],
-                'track_popularity': csv_file[i][3],
-                'genre': csv_file[i][9],
-                'danceability': csv_file[i][11],
-                'energy': csv_file[i][12],
-                'key': csv_file[i][13],
-                'loudness': csv_file[i][14],
-                'speechiness': csv_file[i][16],
-                'acousticness': csv_file[i][17],
-                'instrumentness': csv_file[i][18],
-                'liveness': csv_file[i][19],
-                'valence': csv_file[i][20],
-                'tempo': csv_file[i][21],
-            }
-            dict_list.append(dict)
-
-    return dict_list
+    return [{'track_id': d['id'], 'track_name': d['name'], 'track_artist': d['artist'], 'track_popularity': d['popularity'],
+              'genre': d['genre'], 'danceability': d['danceability'], 'energy': d['energy'], 'key': d['key'],
+              'loudness': d['loudness'], 'speechiness': d['speechiness'], 'acousticness': d['acousticness'],
+              'instrumentness': d['instrumentness'], 'liveness': d['liveness'], 'valence': d['valence'], 'tempo': d['tempo']}
+             for d in csv_file if len(d) == 23]
 
 def input_preference():
-    energy = int(input("Do you like energetic songs? Rate from 1(low) to 3(high)\n"))
-    genre =  input("What kind of genre do you prefer? Choose from: edm ,r&b, latin, rock, rap, pop\n")
-    tempo =  int(input("Do you like fast tempo or slow tempo? Type 1 for fast and 2 for slow\n"))
-    popularity = input("Looking for top track songs or want to discover new songs? Type T or N\n")
+def input_preference():
+    energy = int(input('Do you like energetic songs? Rate from 1(low) to 3(high): '))
+    genre = input('What kind of genre do you prefer? Choose from: edm, r&b, latin, rock, rap, pop: ').lower()
+    tempo = int(input('Do you like fast tempo or slow tempo? Type 1 for fast and 2 for slow: '))
+    popularity = input('Looking for top track songs or want to discover new songs? Type T for top or N for new: ').upper()
     return energy, genre, tempo, popularity
 
 def filter_energy(energy,dict_list):
-    energy_list = []
-    if energy == 1:
-        for dict in dict_list:
-            if float(dict["energy"]) <= 0.3:
-                energy_list.append(dict)
-    elif energy == 2:
-        for dict in dict_list:
-            if float(dict["energy"]) <= 0.6:
-                energy_list.append(dict)
-    elif energy == 3:
-        for dict in dict_list:
-            if float(dict["energy"]) > 0.6:
-                energy_list.append(dict)
-    return energy_list
+def filter_energy(energy, dict_list):
+    energy_thresholds = {1: 0.3, 2: 0.6, 3: float('inf')}
+    return [d for d in dict_list if float(d['energy']) <= energy_thresholds[energy]]
 
 def filter_genre(genre, energy_list):
-    genre_and_energy = []
-    for dict in energy_list:
-        if dict['genre'] == genre:
-            genre_and_energy.append(dict)
-    return genre_and_energy
+    return [d for d in energy_list if d['genre'] == genre]
 
 def filter_tempo(tempo, genre_and_energy):
-    temp_genre_energy = []
-    if tempo == 1:
-        for dict in genre_and_energy:
-            if float(dict["tempo"]) <= 80:
-                temp_genre_energy.append(dict)
-    elif tempo == 2:
-        for dict in genre_and_energy:
-            if float(dict["tempo"]) <= 120:
-                temp_genre_energy.append(dict)
-    elif tempo == 3:
-        for dict in genre_and_energy:
-            if float(dict["tempo"]) > 120:
-                temp_genre_energy.append(dict)
-    return  temp_genre_energy
+    tempo_thresholds = {1: 80, 2: 120, 3: float('inf')}
+    return [d for d in genre_and_energy if float(d['tempo']) <= tempo_thresholds[tempo]]
 
 
-def recommend_song_popular_or_no(popularity, tempo_genre_energy):
+def recommend_song(popularity, tempo_genre_energy):
     if popularity == 'Y':
-        popularity_sorted = sorted(tempo_genre_energy, key = lambda x: x["track_popularity"])
-        return popularity_sorted
+        return sorted(tempo_genre_energy, key=lambda x: x['track_popularity'])
     else:
-        popularity_sorted = sorted(tempo_genre_energy, key = lambda x: x["track_popularity"], reverse = True)
-        return popularity_sorted
-
+        return sorted(tempo_genre_energy, key=lambda x: x['track_popularity'], reverse=True)
 
 
 
 def main():
-    dataset = '/Users/gahyunbaik/Desktop/cs111/free_coding/spotify_songs copy.csv'
+dataset = input('Enter the path to the dataset file: ')
     dataset_list = read_file(dataset)
     dict_data = make_dictionary(dataset_list)
     #print(dict_data)
